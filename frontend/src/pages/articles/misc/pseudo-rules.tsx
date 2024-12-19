9ca8927ae668f977e77e49f25dd235b1b935f286
Kevin Bourbasquet <bourbasquet.k@etik.com>
2024-12-19 18:16:15 +0100

    feat: comment in prevision of futur component
    Await component for annotations.

--- frontend/src/pages/articles/misc/pseudo-rules.tsx
+++ frontend/src/pages/articles/misc/pseudo-rules.tsx
@@ -12,5 +12,6 @@ export default function PseudoRules() {
   return <Article lang='fr' title='Règles de rédaction du pseudo'>
     <Section title='Changer de pseudo' content='Possible toutes les 48h'/>
     <Section title='Nommer son R.E.X.' content='Les caractères spéciaux ne sont pas autorisés'/>
+    {/* À noter: Il est préférable de noter dans un document texte ton pseudo, sinon tu risquerais de ne plus pouvoir te connecter. */}
   </Article>;
 }
